9445e352ee5d5beb0088c85bc5120ea3f7b10ffd
Kleber Vales <klebervales.dev@gmail.com>
2025-08-21 13:42:57 -0300

    Finalize project

--- src/main/java/br/com/example/Usuario.java
+++ src/main/java/br/com/example/Usuario.java
@@ -1,4 +1,29 @@
 package main.java.br.com.example;
 
 public class Usuario {
+    private int id;        // primitivo
+    private String nome;   // referência
+
+    public Usuario(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuário{id=" + id + ", nome='" + nome + "'}";
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Objeto Usuario com id=" + id + " foi destruído pelo Garbage Collector.");
+    }
 }
